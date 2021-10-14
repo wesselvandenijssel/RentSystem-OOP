1a0b4b1659b3d200c7adfc9a91504a2768aa6838
Wessel van den Ijssel <wessel069@gmail.com>
2021-10-14 14:27:23 +0200

    basket

--- glu-base-api/basket/index.php
+++ glu-base-api/basket/index.php
@@ -1,2 +1,18 @@
 <?php
+ require_once("../config/config.php");
+ require_once("../classes/login.php");
+ session_start();
 $id = $_GET["buy"];
+$user_name = $_SESSION['user_name'];
+$sql = "SELECT id, user_name, user_email FROM `users` WHERE user_name = '$user_name'";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["user_name"] . " - Email: ". $row["user_email"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$sql2 = "INSERT INTO basket (user_id, product_id, amount) VALUES (`$row['id']`,`$id`,`1`)";
--- glu-base-api/config/db.php
+++ glu-base-api/config/db.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_NAME", "bibliotheek");
+define("DB_NAME", "api");
 define("DB_USER", "root");
 define("DB_PASS", "");
